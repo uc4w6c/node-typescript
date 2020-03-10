10fa1f3ea0b10868c48afb27d8539dc2048a5572
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-03-10 23:24:31 +0900

    とりあえずURLを取得

--- session-typescript/src/app.ts
+++ session-typescript/src/app.ts
@@ -1,1 +1,16 @@
-console.log('hogehoge');
+import { IncomingMessage, ServerResponse } from 'http';
+
+const http = require('http')
+
+const app = function(req: IncomingMessage, res: ServerResponse, next: Function) {
+    // 暫定でurl出力を置いている
+    console.log('url:' + req.url);
+
+    app.handle(req, res, next);
+};
+app.handle = function handle(req: IncomingMessage, res: ServerResponse, callback: Function): void {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}
+
+http.createServer(app).listen(3000);
