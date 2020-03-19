a62c51a55649a367a4f0031872694ef49767c7fe
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-03-19 23:11:21 +0900

    mysql 実行できた

--- mysql-typescript/src/app.ts
+++ mysql-typescript/src/app.ts
@@ -1,9 +1,35 @@
+/*
+ * mysqljs/mysqlを利用したパターン
+ */
 import * as http from "http";
 import * as mysql from 'mysql';
+import * as util from 'util';
 
-const app = function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port: 3307,
+  user     : 'test',
+  password : 'test',
+  database : 'testdb'
+});
+
+const app = async (req, res) => {
+  connection.connect();
+
+  // @ts-ignore
+  // const query = util.promisify(connection.query);
+  connection.query = util.promisify(connection.query);
+  try {
+    // const data = await query("SELECT Hello AS value");
+    const data = await connection.query("SELECT 'Hello' AS value");
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(data[0].value + ' World!\n');
+    // res.end('Hello World\n');
+  } catch(err) {
+    console.log(err);
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+  }
+  connection.end();
 };
 
 http.createServer(app).listen(3000);
